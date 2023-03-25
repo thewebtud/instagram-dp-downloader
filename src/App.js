88247fa2d4916e5f58b7332c83bbd5db7c8783fb
Ashish Kumar <52043412+thewebtud@users.noreply.github.com>
2023-03-25 10:48:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        I will use this to build Instagram DP Downloader
+        I will use this to build Instagram DP Downloader.
+        Team: Ashish (Developer), Sarmeeeen (UI/UX)
       </header>
     </div>
   );
